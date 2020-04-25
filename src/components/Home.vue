747deee24385e4a7571322e38efcbecc2bbfa80b
oladayosprinble <oladayo.agbolade@sprinble.co>
2020-04-25 10:10:33 +0100

    Feat: Implemented UI

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="col-6">
-    <div class="card" style="width: 18rem;">
-      <img src="#" class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="container">
+    <div class="col-6">
+      <div
+        v-for="(article, index) in articles"
+        :key="index"
+        class="card"
+        style="width: 18rem;"
+      >
+        <img
+          :src="article.urlToImage"
+          class="card-img-top"
+          alt="Image not available"
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{ article.title }}</h5>
+          <p class="card-text"><b>Description:</b> {{ article.description }}</p>
+          <p :v-if="article.author !== null" class="card-text">
+            <b>Author:</b> {{ article.author }}
+          </p>
+          <p class="card-text">
+            <b>Date Published:</b> {{ formatTime(article.publishedAt) }}
+          </p>
+          <a :href="article.url" target="blank" class="btn btn-primary">Read News</a>
+        </div>
       </div>
     </div>
   </div>
@@ -18,17 +32,25 @@
 import axios from "axios";
 export default {
   name: "Home",
-  data(){
-      return{
-        articles: []
-      }
+  data() {
+    return {
+      articles: []
+    };
+  },
+  methods: {
+    formatTime(time) {
+      let newTime = time.split("T");
+      return newTime[0];
+    }
   },
   mounted() {
     const URL = `https://newsapi.org/v2/top-headlines?country=ng&apiKey=ee85542cb07b4ad3997de5cd3f81cb5c`;
-    axios.get(URL)
+    axios
+      .get(URL)
       .then(res => {
         const response = res.data.articles;
         this.articles = response;
+        console.log(this.articles);
       })
       .catch(error => {
         console.log(error);
@@ -39,4 +61,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.card {
+  margin: 20px;
+}
 </style>
