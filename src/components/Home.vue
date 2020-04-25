7e835cadde834f1f2b499b3eb52f66eae7640c16
oladayosprinble <oladayo.agbolade@sprinble.co>
2020-04-25 10:56:11 +0100

    Updated UI made it responsive

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="container">
-    <div class="col-6">
-      <div
-        v-for="(article, index) in articles"
-        :key="index"
-        class="card"
-        style="width: 18rem;"
-      >
-        <img
-          :src="article.urlToImage"
-          class="card-img-top"
-          alt="Image not available"
-        />
-        <div class="card-body">
-          <h5 class="card-title">{{ article.title }}</h5>
-          <p class="card-text"><b>Description:</b> {{ article.description }}</p>
-          <p :v-if="article.author !== null" class="card-text">
-            <b>Author:</b> {{ article.author }}
-          </p>
-          <p class="card-text">
-            <b>Date Published:</b> {{ formatTime(article.publishedAt) }}
-          </p>
-          <a :href="article.url" target="blank" class="btn btn-primary">Read News</a>
+    <div class="row">
+      <div class="articles-container">
+        <div
+          v-for="(article, index) in articles"
+          :key="index"
+          class="card col-md-6"
+        >
+          <img
+            v-if="article.urlToImage !== null"
+            :src="article.urlToImage"
+            class="card-img-top"
+            alt="Image not available"
+          />
+          <img
+            v-else
+            src="https://images.pexels.com/photos/159652/table-food-book-newspaper-159652.jpeg"
+            class="card-img-top"
+            alt="Image not available"
+          />
+          <div class="card-body">
+            <h5 class="card-title">{{ article.title }}</h5>
+            <p class="card-text">
+              <b>Description:</b> {{ article.description }}
+            </p>
+            <p v-if="article.author !== null" class="card-text">
+              <b>Author:</b> {{ article.author }}
+            </p>
+            <p v-else class="card-text">
+              <b>Author:</b> Author name not available
+            </p>
+            <p class="card-text">
+              <b>Date Published:</b> {{ formatTime(article.publishedAt) }}
+            </p>
+            <a :href="article.url" target="blank" class="btn btn-primary"
+              >Read News</a
+            >
+          </div>
         </div>
       </div>
     </div>
@@ -50,7 +65,7 @@ export default {
       .then(res => {
         const response = res.data.articles;
         this.articles = response;
-        console.log(this.articles);
+        // console.log(this.articles);
       })
       .catch(error => {
         console.log(error);
@@ -63,5 +78,18 @@ export default {
 <style scoped>
 .card {
   margin: 20px;
+  width: 600px;
+}
+.articles-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-items: center;
+}
+
+@media only screen and (max-width: 1000px) {
+  .articles-container {
+    margin: 10px;
+    grid-template-columns: repeat(1, 1fr);
+  }
 }
 </style>
